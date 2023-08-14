31922e17135ca86a8ddca94e67a5e33bfad2a5e2
Nathan Mokhtarzadeh <nathanmokh@gmail.com>
2023-08-13 17:41:26 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,9 @@ from pokemon.pokemon import Pokemon
 from pokemon.util.common_utils import get_config
 
 config = get_config()
-engine = create_engine(f"postgresql://{config['db']['user']}:{config['db']['password']}@{config['db']['host']}:{config['db']['port']}/{config['db']['dbname']}")
+engine = create_engine(
+    f"postgresql://{config['db']['user']}:{config['db']['password']}@{config['db']['host']}:{config['db']['port']}/{config['db']['dbname']}"
+)
 
 # Create a session
 Session = sessionmaker(bind=engine)
@@ -15,5 +17,9 @@ session = Session()
 pokemon_data = session.query(Pokemon).all()
 
 for pokemon in pokemon_data:
-    print(f"ID: {pokemon.id}, Name: {pokemon.pokemon_name}, Type 1: {pokemon.type_1}, Type 2: {pokemon.type_2}, Team Role: {pokemon.team_role}")
-    print(f"Stats: HP: {pokemon.stats.hp}, Attack: {pokemon.stats.attack}, Defense: {pokemon.stats.defense}, Speed: {pokemon.stats.speed}")
+    print(
+        f"ID: {pokemon.id}, Name: {pokemon.pokemon_name}, Type 1: {pokemon.type_1}, Type 2: {pokemon.type_2}, Team Role: {pokemon.team_role}"
+    )
+    print(
+        f"Stats: HP: {pokemon.stats.hp}, Attack: {pokemon.stats.attack}, Defense: {pokemon.stats.defense}, Speed: {pokemon.stats.speed}"
+    )
