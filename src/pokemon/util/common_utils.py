06505726b4afcef1cd0df365323dd3d5d993e6f4
Nathan Mokhtarzadeh <nathanmokh@gmail.com>
2023-08-13 17:41:57 -0700

    integrate ORM (#3)
    * implement
    
    * Update models.py
    
    * Update main.py

--- src/pokemon/util/common_utils.py
+++ src/pokemon/util/common_utils.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_config():
-    with open(f"config/{os.environ.get('ENV')}.yaml", "r") as f:
+    with open(f"src/config/{os.environ.get('ENV')}.yaml", "r") as f:
         template = Template(f.read())
         rendered_yaml = template.render(DB_PASSWORD=os.environ.get("DB_PASSWORD"))
     return yaml.safe_load(rendered_yaml)
