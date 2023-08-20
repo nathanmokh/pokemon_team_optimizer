89cca229e6dac258cf328f6e8e6ff9d4c817cd57
Nathan Mokhtarzadeh <nathanmokh@gmail.com>
2023-08-20 00:13:45 -0700

    separate move loading table scripts, add a view for future use

---
+++ src/pokemon/sql/views/get_pokemon_move_set.sql
@@ -0,0 +1,22 @@
+SELECT
+    p.pokemon_name,
+    m.move_name,
+    m.move_type,
+    pmm.learn_method,
+    p.type_1 AS "pokemon type",
+    p.type_2 AS "pokemon_type_2",
+    pmm.level_learned,
+    m.power,
+    m.pp,
+    m.accuracy,
+    m.category,
+    m.crit_rate
+FROM
+    pokemon p
+    JOIN pokemonmovesmapping pmm ON p.id = pmm.pokemon_id
+    JOIN moves m ON m.move_id = pmm.move_id
+WHERE
+    p.id = {pokemon_id}
+    AND pmm.game_version = {game_version}
+ORDER BY
+    pmm.level_learned;
