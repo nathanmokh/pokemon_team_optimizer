d1e2949e19f98d2e9fa0403a8de08053757f19d3
Nathan Mokhtarzadeh <nathanmokh@gmail.com>
2023-08-22 17:54:33 -0700

    more refactors. get rid of unnecessary sql queries

--- src/pokemon/sql/scripts/load_stats_table.py
+++ src/pokemon/sql/scripts/load_stats_table.py
@@ -9,36 +9,36 @@ class StatsTableLoader:
         self.stats_table_creation_script = "create_stats_table.sql"
         self.stats_table_population_script = "populate_stats_table.sql"
 
-    def create_insert_rows(self):
-        currently_loaded_pokemon_stats_ids = self.get_currently_loaded_ids()
+    def _create_insert_rows(self):
+        currently_loaded_pokemon_stats_ids = self._get_currently_loaded_ids()
 
         fetched_data = []
         for pokemon_id in range(1, get_current_number_of_pokemon_pokeapi()):
             if pokemon_id in currently_loaded_pokemon_stats_ids:
                 continue
-            stats = self.get_pokemon_stats(pokemon_id)
+            stats = self._get_pokemon_stats(pokemon_id)
             fetched_data.append((pokemon_id, *stats))
             print(f"Row created for pokemon {pokemon_id}")
-        return self.format_insert_rows(fetched_data)
+        return self._format_insert_rows(fetched_data)
 
-    def get_pokemon_stats(self, pokemon_id):
+    def _get_pokemon_stats(self, pokemon_id):
         URL = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}"
         stats = requests.get(URL).json()["stats"]
         return [stat["base_stat"] for stat in stats]
 
-    def get_currently_loaded_ids(self):
+    def _get_currently_loaded_ids(self):
         return [
             record[0] for record in execute_sql(raw_sql="SELECT pokemon_id FROM stats;")
         ]
 
-    def format_insert_rows(self, rows):
+    def _format_insert_rows(self, rows):
         return ", ".join(str(tuple(row)) for row in rows)
 
     def run(self):
         execute_sql(self.stats_table_creation_script)
         # get currently loaded IDs to prevent creating duplicate rows
         # create rows
-        rows = self.create_insert_rows()
+        rows = self._create_insert_rows()
 
         logging.info(f"Loading pokemon base stats to table, {len(rows)} rows.")
         print(f"Loading pokemon base stats to table, {len(rows)} rows.")
